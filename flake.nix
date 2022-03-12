49fcc909a33de172a4bdd93d0620cbf0bdf0f50c
hugosenari <hugosenari@gmail.com>
2022-03-12 02:40:30 -0300

    chore: re index rfc3339

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''RFC3339 (dates and times) implementation for Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."rfc3339-master".url = "path:./master";
+  inputs."rfc3339-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."rfc3339-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
